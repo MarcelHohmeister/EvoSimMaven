4bd255edadb3353f1f314bf71f0edc297b5aeef0
Marcel Hohmeister <ma.hohmeister@gmail.com>
2026-07-22 23:30:33 +0200

    feat: add IDManager

---
+++ src/main/java/net/hohmeima/simulation/ecs/ids/IDManager.java
@@ -0,0 +1,19 @@
+package net.hohmeima.simulation.ecs.ids;
+
+import java.util.ArrayDeque;
+
+public class IDManager
+{
+	private final static ArrayDeque<Integer> ids = new ArrayDeque<>();
+	private int highestID = 0;
+
+	public int getID()
+	{
+		return ids.isEmpty() ? highestID++ : ids.pop();
+	}
+
+	public void returnID(final int id)
+	{
+		ids.addLast(id);
+	}
+}
