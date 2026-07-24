73811d686e87260c46dbb0e03eee563e78b24555
Marcel Hohmeister <ma.hohmeister@gmail.com>
2026-07-25 00:55:00 +0200

    chore: restructure Settings class with region and NS_PER_TICK constant

--- src/main/java/net/hohmeima/evosim/core/Settings.java
+++ src/main/java/net/hohmeima/evosim/core/Settings.java
@@ -2,5 +2,8 @@ package net.hohmeima.evosim.core;
 
 public class Settings
 {
+	//region FixedUpdate
 	public final static int TARGET_TPS = 60;
+	private final static long NS_PER_TICK = 1_000_000_000L / TARGET_TPS;
+	//endregion
 }
