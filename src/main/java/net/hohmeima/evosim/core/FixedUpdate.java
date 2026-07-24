f33c33ea10da670bc2c39b06f39cc291da4f69d1
Marcel Hohmeister <ma.hohmeister@gmail.com>
2026-07-25 00:48:34 +0200

    feat: Calculate Nanoseconds per Tick from Settings.TARGET_TPS

--- src/main/java/net/hohmeima/evosim/core/FixedUpdate.java
+++ src/main/java/net/hohmeima/evosim/core/FixedUpdate.java
@@ -4,6 +4,9 @@ public class FixedUpdate
 {
 	private boolean running = true;
 
+	private final static long NS_PER_TICK = 1_000_000_000L / Settings.TARGET_TPS;
+	
+
 	public void start()
 	{
 		while (running)
