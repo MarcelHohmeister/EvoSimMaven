3280be12f3232e79623121c6efdc61fb89b33726
Marcel Hohmeister <ma.hohmeister@gmail.com>
2026-07-25 00:39:56 +0200

    feat: Add FixedUpdate structure

---
+++ src/main/java/net/hohmeima/evosim/core/FixedUpdate.java
@@ -0,0 +1,19 @@
+package net.hohmeima.evosim.core;
+
+public class FixedUpdate
+{
+	private boolean running = true;
+
+	public void start()
+	{
+		while (running)
+		{
+
+		}
+	}
+
+	public void stop()
+	{
+		running = false;
+	}
+}
