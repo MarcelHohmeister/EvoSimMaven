2ce15251c1d11a1aa0a90cf0d2e946c9b3ecc639
Marcel Hohmeister <ma.hohmeister@gmail.com>
2026-07-26 01:46:20 +0200

    feat: add ComponentManager core and PositionComponent

---
+++ src/main/java/net/hohmeima/evosim/simulation/ecs/components/physics/PositionComponent.java
@@ -0,0 +1,8 @@
+package net.hohmeima.evosim.simulation.ecs.components.physics;
+
+import net.hohmeima.evosim.core.Vector2D;
+
+public class PositionComponent
+{
+	public final Vector2D position = new Vector2D(0, 0);
+}
