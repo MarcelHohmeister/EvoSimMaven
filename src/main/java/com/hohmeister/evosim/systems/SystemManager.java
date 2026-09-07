234ceda15b4114009de1a3af1487daf76cd4fc9b
Marcel <ma.hohmeister@gmail.com>
2026-09-07 15:33:58 +0200

    Add SystemManager

--- src/main/java/com/hohmeister/evosim/systems/SystemManager.java
+++ src/main/java/com/hohmeister/evosim/systems/SystemManager.java
@@ -1,5 +1,12 @@
 package com.hohmeister.evosim.systems;
 
-public class SystemManager {
+import java.util.ArrayList;
+import java.util.List;
 
+public class SystemManager implements Tickable {
+    public final List<Tickable> allSystems = new ArrayList<>();
+
+    public void tick(final double dt) {
+        allSystems.forEach(system -> system.tick(dt));
+    }
 }
