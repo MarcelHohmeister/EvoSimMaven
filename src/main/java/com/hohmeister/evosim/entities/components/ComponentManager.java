91f9df28865edad362e3b49a21298dc5196f6ff9
Marcel <ma.hohmeister@gmail.com>
2026-09-07 23:43:21 +0200

    [feat] add HydrationComponent and PositionComponent to ComponentManager

--- src/main/java/com/hohmeister/evosim/entities/components/ComponentManager.java
+++ src/main/java/com/hohmeister/evosim/entities/components/ComponentManager.java
@@ -1,5 +1,8 @@
 package com.hohmeister.evosim.entities.components;
 
+import com.hohmeister.evosim.entities.components.physics.PositionComponent;
+import com.hohmeister.evosim.entities.components.urgencies.HydrationComponent;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -7,10 +10,13 @@ import java.util.List;
 public class ComponentManager {
     public List<HashMap<Integer, ? extends EntityComponent>> allComponents = new ArrayList<>();
 
-    // Add Component
-    //public final HashMap<Integer, HydrationComponent> hydrationComponents = newComponent();
+    // Physics Components
+    public final HashMap<Integer, PositionComponent> positionComponents = newComponentMap();
 
-    public <T extends EntityComponent> HashMap<Integer, T> newComponent() {
+    // Urgency Components
+    public final HashMap<Integer, HydrationComponent> hydrationComponents = newComponentMap();
+
+    public <T extends EntityComponent> HashMap<Integer, T> newComponentMap() {
         final HashMap<Integer, T> newComponent = new HashMap<>();
         allComponents.add(newComponent);
         return newComponent;
