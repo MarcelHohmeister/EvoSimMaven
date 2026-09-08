c9e1eb11cbf640af7b57c93293d1b0fb10cac1d9
Marcel <ma.hohmeister@gmail.com>
2026-09-08 14:54:55 +0200

    [feat] add SpawnRequest and HydrationRawData classes to blueprint package

---
+++ src/main/java/com/hohmeister/evosim/lifecycle/blueprint/HydrationRawData.java
@@ -0,0 +1,4 @@
+package com.hohmeister.evosim.lifecycle.blueprint;
+
+public record HydrationRawData(double maxHydration, double decayPerSecond, double awarenessThresholdPercent) {
+}
---
+++ src/main/java/com/hohmeister/evosim/lifecycle/blueprint/SpawnRequest.java
@@ -0,0 +1,8 @@
+package com.hohmeister.evosim.lifecycle.blueprint;
+
+import com.hohmeister.evosim.math.Vector2D;
+
+public class SpawnRequest {
+    public Vector2D position;
+    public HydrationRawData hydrationData;
+}
